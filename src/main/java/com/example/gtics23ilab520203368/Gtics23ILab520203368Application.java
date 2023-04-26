8637cf95149a4ee6a7b541974d4a76f1f0764b47
sebas1310 <a20203369@pucp.edu.pe>
2023-04-25 20:36:06 -0500

    main

---
+++ src/main/java/com/example/gtics23ilab520203368/Gtics23ILab520203368Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics23ilab520203368;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gtics23ILab520203368Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gtics23ILab520203368Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics23ilab520203368/Gtics23ILab520203368ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics23ilab520203368;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gtics23ILab520203368ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
